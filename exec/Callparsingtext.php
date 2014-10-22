7c73212f48ce244ea58c7f5728aad72844f09fda
Samurai <vikky_manit@yahoo.co.in>
2014-10-22 12:19:01 +0200

    Update c++ file to include sum

--- exec/Callparsingtext.php
+++ exec/Callparsingtext.php
@@ -1,5 +1,5 @@
 <?php
-exec("/var/www/parsing data.txt",$json);
+exec("/var/www/ScTeam11/parsing data.txt",$json);
 $json = implode($json);
 //$body = '[{"srcacc":123,"destacc":345,"amount":1,"tan":"a123sA"}, {"srcacc":198,"destacc":234,"amount":3,"tan":"aoefj1"}]';
 //print_r($body);
@@ -7,13 +7,12 @@ $json = implode($json);
 
 $json=json_decode($json);
 //echo $json[0]->srcacc;
-print_r ($json);
-for ($i=0; $i<sizeof($json); $i++) {
-	$srcacc[$i] = $json[$i]->srcacc;
+//print_r ($json);
+for ($i=0; $i<sizeof($json) - 1; $i++) {
 	$destacc[$i] = $json[$i]->destacc;
 	$amount[$i] = $json[$i]->amount;
-	$tan[$i] = $json[$i]->tan;
 }
-print_r($tan);
+$sum = $json[sizeof($json)-1];
+print_r ($sum);
 
 ?>
