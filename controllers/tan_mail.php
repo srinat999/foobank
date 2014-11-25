8d03786a05bb9a741bfb5ab3071d329e34c51089
root <shivguru.rao.91@gmail.com>
2014-11-25 22:02:26 +0100

    Integreated pdf with password protection

--- controllers/tan_mail.php
+++ controllers/tan_mail.php
@@ -1,14 +1,34 @@
 <?php
-	function tan_mail($TANList,$email,$accountNo){
-	$email_subject = "Your TAN List";
-	$email_address = $email;
-	$email_message = "Dear Customer,". "\n\n" ."We have activated your account. Please find your Account Number and TAN List below.\n";
-	$email_message .= "\n Account Number : " . $accountNo . "\nYour Tan List:\n"; 
-	for($i=0;$i<100;$i++)
-	{
-		$email_message .= $i+1 . "->" . $TANList[$i]. "\n";        //The TAN list
-	}	
-	$email_message .= "\n\n Thank You for banking with us.";
-	mail($email_address,$email_subject,$email_message,'From: securebankingcode@gmail.com');
-	}
+require_once('../lib/PHPMailer/PHPMailerAutoload.php');
+function tan_mail($email_address,$accountNo)
+{
+    $mail = new PHPMailer(); // defaults to using php "mail()"
+    
+    $body = "<p>Dear Customer,<br><br> Your account has now been activated. Your account number is '$accountNo'.<br><br>
+		Please find your attached TAN list. The list is password protected. 
+		The password is combination of first four characters of your full name in uppercase and first four characters of your
+		username in lowercase. For example, if your full name is John Watson and username is jwatson then your password will be JOHNjwat.
+		<br><br> Thank You for banking with us.</p>";
+    
+    $mail->SetFrom('securebankingcode@gmail.com', 'TUM International Bank');
+    
+    //$mail->AddReplyTo("name@yourdomain.com","First Last");
+    
+    $address = $email_address;
+    $mail->AddAddress($address);
+    $mail->Subject = "TAN List from TUM International Bank";
+    $mail->AltBody = "To view the message, please use an HTML compatible email viewer!"; // optional, comment out and test		
+    $mail->MsgHTML($body);
+    $mail->AddAttachment("TANList.pdf"); // attachment
+    
+//    		 if(!$mail->Send()) {
+//    		   echo "Mailer Error: " . $mail->ErrorInfo;
+//    		 } else {
+//   		   echo "Message sent!";
+//    		 }
+
+    unlink("TANList.pdf");
+}
+//echo tan_mail("vikky_manit@yahoo.co.in");
 ?>
+
