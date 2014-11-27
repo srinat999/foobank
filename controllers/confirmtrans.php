f106ab267015470201c368049613bc85d5c2c106
Sreenath Prem <srinat999@gmail.com>
2014-11-27 18:59:14 +0100

    SCS implementation for transaction

--- controllers/confirmtrans.php
+++ controllers/confirmtrans.php
@@ -8,21 +8,32 @@ if(!isSessionActive() || !enforceRBAC('customer')) {
 	die();
 }
 $userid=$_SESSION['uid'];
-
-$tan=$_POST["tan"];
-$tanInSession=$_SESSION['tan'];
-if ($tanInSession!=$tan) {
-	mysql_close($con);
-	$_SESSION['error']=3;
-	header("Location: ../view/error.php");
-	die();
-}
-
 $src_account=$_SESSION['src_account'];
 $dst_account=$_SESSION['dst_account'];
 $amount=$_SESSION['amount'];
 $dst_userid=$_SESSION['dst_userid'];
 $description=$_SESSION['description'];
+
+$tan=$_POST["tan"];
+$tanInSession=$_SESSION['tan'];
+$clientPIN = $_SESSION['clientPIN'];
+if (($_SESSION['tranauth']=='email') && ($tanInSession!=$tan)) {
+	mysql_close($con);
+	$_SESSION['error']=3;
+	header("Location: ../view/error.php");
+	die();
+} elseif ($_SESSION['tranauth']=='scs') {
+	$result = shell_exec("java -jar ../securToken/securToken.jar $clientPIN $amount $dst_account $tan");
+	if ($result=='false') {
+		mysql_close($con);
+		$_SESSION['error']=3;
+		header("Location: ../view/error.php");
+		die();
+	}
+} else {
+	
+}
+
 mysql_query("UPDATE tan_numbers SET expired=1 WHERE tan='$tan'");
 submitTrans($src_account, $dst_account, $amount, $userid, $dst_userid, $description);
 mysql_close($con);
